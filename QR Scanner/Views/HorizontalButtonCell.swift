216434a22bcc84df019ce70fc2f78e50493278d3
swaruppanda11 <swaruppanda11@gmail.com>
2024-11-21 19:56:29 +0530

    gradient of buttons in horizontal scroll view fixed

--- QR Scanner/Views/HorizontalButtonCell.swift
+++ QR Scanner/Views/HorizontalButtonCell.swift
@@ -41,14 +41,48 @@ class HorizontalButtonCell: UICollectionViewCell {
     func configure(with title: String, isSelected: Bool) {
         buttonLabel.text = title
         
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1).cgColor,
+            UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1).cgColor,
+            UIColor.black.cgColor
+        ]
+        gradientLayer.locations = [0.0, 0.5, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.2, y: 0.4)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradientLayer.frame = contentView.bounds
+        gradientLayer.cornerRadius = contentView.layer.cornerRadius
+        gradientLayer.name = "ShinyGradient"
+        
         if isSelected {
-            contentView.backgroundColor = .white
-            buttonLabel.textColor = .black
-            contentView.layer.borderColor = UIColor.clear.cgColor
-        } else {
-            contentView.backgroundColor = UIColor(red: 35/255, green: 45/255, blue: 60/255, alpha: 1.0)
+            contentView.backgroundColor = UIColor(red: 35/255, green: 45/255, blue: 60/255, alpha: 1)
             buttonLabel.textColor = .white
+            contentView.layer.borderWidth = 1.0
             contentView.layer.borderColor = UIColor(red: 1.0, green: 1.0, blue: 0.5, alpha: 1.0).cgColor
+            gradientLayer.colors = [
+                UIColor(red: 60/255, green: 60/255, blue: 40/255, alpha: 1).cgColor,
+                UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1).cgColor,
+                UIColor.black.cgColor
+            ]
+            
+            removeExistingGradientLayer()
+            contentView.layer.insertSublayer(gradientLayer, at: 0)
+        } else {
+            contentView.backgroundColor = UIColor(red: 35/255, green: 45/255, blue: 60/255, alpha: 0.5)
+            buttonLabel.textColor = .white
+            contentView.layer.borderWidth = 1.0
+            contentView.layer.borderColor = UIColor(red: 1.0, green: 1.0, blue: 0.5, alpha: 0.4).cgColor
+            
+            removeExistingGradientLayer()
+            contentView.layer.insertSublayer(gradientLayer, at: 0)
+        }
+    }
+
+    private func removeExistingGradientLayer() {
+        if let sublayers = contentView.layer.sublayers {
+            for layer in sublayers where layer.name == "ShinyGradient" {
+                layer.removeFromSuperlayer()
+            }
         }
     }
 }
